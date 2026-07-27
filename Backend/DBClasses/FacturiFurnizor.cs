c743ac3808e7760eecbab1c5e42281af48b154b0
Rares-Alexandrescu <rares.alexan2005@gmail.com>
2026-07-27 18:13:26 +0300

    Am schimbat putin baza de date, ca sa fie facturile la furnizor mult mai ok, logica de en grosse

--- Backend/DBClasses/FacturiFurnizor.cs
+++ Backend/DBClasses/FacturiFurnizor.cs
@@ -4,14 +4,11 @@ namespace Backend.DBClasses
     {
         public int facturi_id { get; set; } = -1;
         public int furnizor_id { get; set; } = -1;
-        public int comanda_piese_id { get; set; } = -1;
 
-        public decimal pret_brut { get; set; } = 0m;
+        public decimal pret_total_brut { get; set; } = 0m;
 
+        public string? path_factura_pdf { get; set; } = string.Empty;
         public bool? stadiu_plata { get; set; } = false;
-        public bool? stadiu_logistica { get; set: } = false;
-
-        public string? path_factura_furnizor { get; set; } = string.Empty;
 
         public DateTime created_at { get; set; } = DateTime.Now;
     }
---
+++ Backend/DBClasses/FacturiFurnizorLinie.cs
@@ -0,0 +1,13 @@
+namespace Backend.DBClasses
+{
+    public class FacturiFurnizorLinie
+    {
+        public int factura_linie_id { get; set; } = -1;
+        public int comanda_piese_id{ get; set; } = -1;
+        public int facturi_id { get; set; } = -1;
+
+        public decimal pret_brut { get; set; } = 0m;
+
+        public bool? stadiu_logistica { get; set; } = false;
+    }
+}
